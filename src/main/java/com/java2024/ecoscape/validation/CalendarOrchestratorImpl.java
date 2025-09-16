ed1fcbf92bfeb0ba9be50fb78a85481f659cc1ee
Shams Alsaffar <shamsay_1985@yahoo.com>
2025-09-17 00:26:35 +0200

    chore: pom + fix CalendarOrchestratorImpl (mapper setup)

--- src/main/java/com/java2024/ecoscape/validation/CalendarOrchestratorImpl.java
+++ src/main/java/com/java2024/ecoscape/validation/CalendarOrchestratorImpl.java
@@ -6,6 +6,8 @@ import com.java2024.ecoscape.services.ListingAvailableDatesService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.java2024.ecoscape.exceptions.BusinessValidationException;
+
 import java.time.LocalDate;
 
 @Component
@@ -46,7 +48,7 @@ public class CalendarOrchestratorImpl implements CalendarOrchestrator {
     public void tryRescheduleOrThrow (Listing listing, Booking booking, LocalDate newStart, LocalDate newEnd ) {
         boolean available = listingAvailableDatesService.checkAvailability(listing.getId(), newStart, newEnd);
         if (!available) {
-            throw new com.java2024.ecoscape.validation.BusinessValidationException(
+            throw new BusinessValidationException(
                     java.util.List.of("The listing is unavaliable for the requested dates.")
             );
         }
