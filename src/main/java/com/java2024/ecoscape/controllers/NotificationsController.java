0ec63624c9f3d12268d6ddf9af6771a4fb9f1bae
Iasmina Alkashash <iasminakash@hotmail.com>
2025-09-21 21:50:16 +0200

    made some further changes to make notificationDTO being sent with ID, in order to changes it's seen status in future, send the creation date and well

---
+++ src/main/java/com/java2024/ecoscape/controllers/NotificationsController.java
@@ -0,0 +1,26 @@
+package com.java2024.ecoscape.controllers;
+
+import com.java2024.ecoscape.dto.NotificationDTO;
+import com.java2024.ecoscape.websockets.service.PushNotificationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationsController {
+    private final PushNotificationService notificationService;
+
+    public NotificationsController(PushNotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<NotificationDTO>> requestHost(@PathVariable(name = "userId") Long userId) {
+        return ResponseEntity.ok(notificationService.getAllNotificationsByUserId(userId));
+    }
+}
