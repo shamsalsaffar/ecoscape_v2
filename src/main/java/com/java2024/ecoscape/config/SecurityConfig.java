26f8ab55c9ca2425fd7d4d7c5fe7a5e43095fb96
alex <nalleballeuwu@gmail.com>
2025-09-23 10:40:34 +0200

    added patch to CORS so frontend can use it

--- src/main/java/com/java2024/ecoscape/config/SecurityConfig.java
+++ src/main/java/com/java2024/ecoscape/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cookie"));
 
         configuration.setAllowCredentials(true);
